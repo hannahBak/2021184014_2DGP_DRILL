ca0cce4054a74485d72b56210156f90e70273fbb
hannahBak <gosk031240@gmail.com>
2022-09-21 17:40:25 +0900

    drill 06 완성
    2021184014 박해나

--- 06/DRILL#06.py
+++ 06/DRILL#06.py
@@ -6,36 +6,37 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('character.png')
 
-x = 0
-while (x < 800):
-    clear_canvas_now()
-    grass.draw_now(400,30)
-    character.draw_now(x,90)
-    x = x + 2
-    delay(0.01)
-y = 0
-while (y < 600):
-    clear_canvas_now()
-    grass.draw_now(400,30)
-    character.draw_now(x,y)
-    y = y + 2
-    delay(0.01)
+def rectangle():
+    x = 0
+    while (x < 800):
+        clear_canvas_now()
+        grass.draw_now(400,30)
+        character.draw_now(x,90)
+        x = x + 2
+        delay(0.01)
+    y = 90
+    while (y < 600):
+        clear_canvas_now()
+        grass.draw_now(400,30)
+        character.draw_now(x,y)
+        y = y + 2
+        delay(0.01)
     
-while (x > 0):
-    clear_canvas_now()
-    grass.draw_now(400,30)
-    character.draw_now(x,y)
-    x = x - 2
-    delay(0.01)
+    while (x > 0):
+        clear_canvas_now()
+        grass.draw_now(400,30)
+        character.draw_now(x,y)
+        x = x - 2
+        delay(0.01)
 
-while (y > 90):
-    clear_canvas_now()
-    grass.draw_now(400,30)
-    character.draw_now(x,y)
-    y = y - 2
-    delay(0.01)
+    while (y > 90):
+        clear_canvas_now()
+        grass.draw_now(400,30)
+        character.draw_now(x,y)
+        y = y - 2
+        delay(0.01)
 
-
+while(1):
+    rectangle()
     
-
 close_canvas()
