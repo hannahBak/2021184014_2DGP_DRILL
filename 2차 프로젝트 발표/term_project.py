9bbe7451118b9f753be235df97ddc0fe2d523749
hannahBak <gosk031240@gmail.com>
2022-10-14 00:22:50 +0900

    애니메이션 수정 및 좌우 이동 구현

--- 2차 프로젝트 발표/term_project.py
+++ 2차 프로젝트 발표/term_project.py
@@ -2,9 +2,18 @@ from pico2d import *
 
 bath_WIDTH, bath_HEIGHT = 720, 540
 
+open_canvas(bath_WIDTH, bath_HEIGHT)
+
+bath = load_image('bath.png')
+character1 = load_image('myCharacter1.png')
+character2 = load_image('myCharacter2.png')
+
+p = character1
+
 def handle_events():
     global running
     global dir
+    global p, character1, character2
     events = get_events()
 
     for event in events:
@@ -13,8 +22,12 @@ def handle_events():
         elif event.type == SDL_KEYDOWN:
             if event.key == SDLK_RIGHT:
                 dir += 1
+                p = character1
+                
             elif event.key == SDLK_LEFT:
                 dir -= 1
+                p = character2
+               
             elif event.key == SDLK_ESCAPE:
                 running = False
         elif event.type == SDL_KEYUP:
@@ -23,10 +36,6 @@ def handle_events():
             elif event.key == SDLK_LEFT:
                 dir += 1
 
-open_canvas(bath_WIDTH, bath_HEIGHT)
-
-bath = load_image('bath.png')
-character = load_image('myCharacter1.png')
 
 running = True
 x = 720 // 2
@@ -36,11 +45,11 @@ dir = 0
 while running:
     clear_canvas()
     bath.draw(bath_WIDTH // 2, bath_HEIGHT // 2)
-    character.clip_draw(frame * 97, 420, 80, 150, x, 90)
+    p.clip_draw(frame * 120, 420, 115, 150, x, 90)
     update_canvas()
     handle_events()
     frame = (frame + 1) % 6
-    x += dir * 5
+    x += dir * 25
     delay(0.1)
 
 close_canvas()
